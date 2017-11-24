615ed9ce76c9d51ea7598b0620bc63f11da6d384
jphaneuf <phaneuf.joe@gmail.com>
2017-11-24 14:36:23 -0500

    main takes in n labels, labels, passes to camera

--- data_acquisition_live/src/camera.py
+++ data_acquisition_live/src/camera.py
@@ -1,58 +1,37 @@
 import cv2
 import os
 import re
-import time 
-import sys
+#import time 
+#import sys
 from threading import Thread,Event
-
-
 
 ########CHANGE THIS UNTIL YOU FIND THE RIGHT CAMERA##############
 CameraNum = 0
-#######ADD OR REMOVE REQUIRED LABEL CLASSES###################
-Labels = ["fairway", "rough", "mixed", "sidewalk", "dirt"]
-
 
 class Camera(Thread):
-  def __init__(self,img_dir,event):
+  def __init__(self,img_dir,event,capture_rate=5):
     Thread.__init__(self)
     self.stopped = event
-    global Labels
-    self.grassLabels = Labels
-    #self.directory_name = "./dataz"
-    self.capRate = 10
     global CameraNum
     self.cap = cv2.VideoCapture(CameraNum)
     self.img_dir = img_dir
+    self.delay = 1 / float(capture_rate)
 
   def run(self):
+    #while not self.stopped.wait(self.delay):
     while not self.stopped.wait(1):
-      print self.get_img_count()
+      print 'oh hey cameraing'
       self.capture()
-      print("my thread")
+
+  def set_dir(self,img_dir):
+    self.img_dir = img_dir
 
   def capture(self):
     _, frame = self.cap.read()
     cv2.imshow("Robostats Data Collector", frame)
     cv2.waitKey(1)
-    #if os.path.exists(self.directory_name.get()):
-    #if os.path.exists(self.directory_name):
-    #labelDir = self.directory_name + "/" + "mylabelz" #self.grassLabel.get()
-    #if not os.path.exists(labelDir):
-    #  os.makedirs(labelDir)
-
-    #check existing files
-    #listOfFiles = [f for f in os.listdir(labelDir)]
-    #print listOfFiles
-    #maxNum = 0
-    #if len(listOfFiles) > 0:
-    #  fileNums = [int(re.search(r'\d+', n).group()) for n in listOfFiles]
-    #  maxNum = max(fileNums)
-
-    #filename = labelDir + "/" + "mylabelz" + str(maxNum + 1) + ".png"
     filename = "{}/img_{}.jpg".format(self.img_dir,self.get_img_count()+1)
     cv2.imwrite(filename, frame)
-
 
   def get_img_count(self):
     files = []
--- data_acquisition_live/src/main.py
+++ data_acquisition_live/src/main.py
@@ -2,14 +2,60 @@ from threading import Thread,Event
 from camera import Camera
 import os
 
+#####Config
+##Todo: load config json
 output_dir = "./dataz/"
-if not os.path.exists(output_dir):
-  os.mkdir(output_dir)
+capture_rate = 5 #Hz donut?
+##########
 
 
-stopFlag = Event()
-camera = Camera(output_dir,stopFlag)
-camera.start()
-# this will stop the timer
-#stopFlag.set()
-#stopFlag.clear()
+class Classifier():
+  def __init__(self):
+
+    self.commands = {'c':self.classify,'s':self.stop}
+
+    self.img_dir = output_dir
+    self.labels = []
+    n_labels = int(raw_input('How many grudslabels would you like?'))
+    for i in range(n_labels):
+      label = raw_input('label {}:'.format(i + 1))
+      self.labels.append(label)
+      label_dir = os.path.join(output_dir,label)
+      self.commands[str(i+1)] = lambda d= self.get_dir(i): self.capture(d)
+      if not os.path.exists(label_dir):
+        os.mkdir(label_dir)
+
+    self.camera_disable = Event()
+    #self.camera_disable.set()
+    self.camera = Camera(output_dir,self.camera_disable,capture_rate=capture_rate)
+    self.camera.start()
+
+  def get_dir(self,img_n):
+    return os.path.join(self.img_dir,self.labels[img_n])
+
+  def capture(self,img_dir):
+    self.camera.set_dir(img_dir)
+    self.camera_disable.clear()
+
+  def classify(self):
+    print 'classifying:todo'
+
+  def batch_train(self):
+    print 'batch train: todo'
+
+  def stop(self):
+    print 'stopping'
+    self.camera_disable.set()
+    #todo:Disable classifier
+
+  def run(self):
+    while 1:
+      command = raw_input('What is thy bidding?:')
+      try:
+        self.commands[command]()
+      except KeyError:
+        print '{} not in commands'.format(command)
+  
+classifier = Classifier()
+classifier.run()
+exit()
